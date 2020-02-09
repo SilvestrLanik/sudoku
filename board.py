3019685b08ac7bf225f41a28bcf5191f2e4decea
Silvestr Lanik <laniksil@fit.cvut.cz>
2020-02-09 14:19:23 +0100

    Initial commit
    Creating class board

---
+++ board.py
@@ -0,0 +1,21 @@
+import math
+
+# i did not find any easy way to make variable const in python
+EMPTY = 0
+
+class Board:
+    #TODO comment size
+    def __init__(self, size):
+        self.grid = [[EMPTY] * size for i in range(size)]
+        self.size = size
+        self.square_size = math.sqrt(self.size)  #size of inner square, for traditional sudoku it is 3
+
+    def print_board(self):
+        for i in range(self.size):
+            for j in range(self.size):
+                print(self.grid[i][j], " ", end='')  # prevent from printing each number on different line
+                if j % self.square_size == self.square_size - 1:
+                    print("|  ", end='')
+            print()
+            if i % self.square_size == self.square_size - 1:
+                print("-----------------------------------")  # for bigger sudoku it must be rewritten to loop 
---
+++ main.py
@@ -0,0 +1,10 @@
+from Sudoku.board import Board
+
+
+def main():
+    sudoku = Board(9)
+    sudoku.print_board()
+
+
+if __name__ == '__main__':
+    main()
